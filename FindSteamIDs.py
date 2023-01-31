e3426f3e09b55c1121ec870eaf92da1d873009ea
Richard Gerard Marcoux III <RMarcouxIII@gmail.com>
2023-01-30 20:29:29 -0500

    none

--- FindSteamIDs.py
+++ FindSteamIDs.py
@@ -33,50 +33,64 @@ for game in response["response"]["games"]:
     #dont overload the steam api
     time.sleep(1.6)
     #get the game app details
-    app_details_response = session.get("https://store.steampowered.com/api/appdetails?appids=" + str(game["appid"]))
+    app_details_response = session.get("https://store.steampowered.com/api/appdetails?appids=" + str(game["appid"]) + "&filters=categories,basic")
     #check if call was successful
     if app_details_response.status_code == requests.codes.ok:
         #convert the response to json
         app_details = app_details_response.json()
+        #get the game name
+        game_name = "Unknown Game"
+        if "name" in game:
+            game_name = game["name"]
+        #convert game name to a valid file name
+        game_name = game_name.replace(":", "")
+        game_name = game_name.replace("/", "")
+        game_name = game_name.replace("\\", "")
+        game_name = game_name.replace("*", "")
+        game_name = game_name.replace("?", "")
+        game_name = game_name.replace("\"", "")
+        game_name = game_name.replace("<", "")
+        game_name = game_name.replace(">", "")
+        game_name = game_name.replace("|", "")
 
         #check if call was successful
         if app_details[str(game["appid"])]["success"] == True:
-            #get the game name
-            game_name = app_details[str(game["appid"])]["data"]["name"]
-            #convert game name to a valid file name
-            game_name = game_name.replace(":", "")
-            game_name = game_name.replace("/", "")
-            game_name = game_name.replace("\\", "")
-            game_name = game_name.replace("*", "")
-            game_name = game_name.replace("?", "")
-            game_name = game_name.replace("\"", "")
-            game_name = game_name.replace("<", "")
-            game_name = game_name.replace(">", "")
-            game_name = game_name.replace("|", "")
+            
 
-            game_controller_support = "none"
-            #get the game steam id
-            game_steam_id = game["appid"]
-            #check if controller support is included in the app details
-            if "controller_support" in app_details[str(game["appid"])]["data"]:
-                #get the game controller support
-                game_controller_support = app_details[str(game["appid"])]["data"]["controller_support"]
-            print(game_name + ": Controller Support: " + str(game_controller_support))
-            #if the game has full controller support
-            if game_controller_support == "full":
-                #create a file url shortcut for the game
-                #create the directory if it doesn't exist
-                if not os.path.exists(".\\ControllerGames"):
-                    os.makedirs(".\\ControllerGames")
-                #create the shortcut file if it doesn't exist
-                if not os.path.exists(".\\ControllerGames\\" + game_name + ".url"):
-                    file = open(".\\ControllerGames\\" + game_name + ".url", "w")
-                    file.write("[InternetShortcut]\n")
-                    file.write("URL=steam://rungameid/" + str(game_steam_id))
-                    file.close()
+            #get the categories of the game if they exist
+            if "categories" in app_details[str(game["appid"])]["data"]:
+                game_categories = app_details[str(game["appid"])]["data"]["categories"]
+
+                #default controller support to none
+                game_controller_support = "none"
+                #get the steam id of the game
+                game_steam_id = game["appid"]
+                
+                #check for controller support
+                for category in game_categories:
+                    if category["id"] == 18:
+                        game_controller_support = "partial"
+                        break
+                    if category["id"] == 28:
+                        game_controller_support = "full"
+                        break
+
+                print(game_name + ": Controller Support: " + str(game_controller_support))
+                #if the game has full controller support
+                if game_controller_support == "full":
+                    #create a file url shortcut for the game
+                    #create the directory if it doesn't exist
+                    if not os.path.exists(".\\ControllerGames"):
+                        os.makedirs(".\\ControllerGames")
+                    #create the shortcut file if it doesn't exist
+                    if not os.path.exists(".\\ControllerGames\\" + game_name + ".url"):
+                        file = open(".\\ControllerGames\\" + game_name + ".url", "w")
+                        file.write("[InternetShortcut]\n")
+                        file.write("URL=steam://rungameid/" + str(game_steam_id))
+                        file.close()
         else:
             #print the error code and the description
-            print("Error: " + str(app_details_response.status_code) + " " + app_details_response.reason)
+            print("Error: app details not found for: " + game_name)
     else:
         #print the error code abd the description
         print("Error: " + str(app_details.status_code) + " " + app_details.reason)
